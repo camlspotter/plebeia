c0a168ff24bef326588ffe2a2cc6081625955697
Jun Furuse <jun.furuse@gmail.com>
2019-02-22 01:33:43 +0000

    Debug.validate_node now checks some attributes. (Though their correctness should be assured by the type system)

--- src/debug.ml
+++ src/debug.ml
@@ -75,47 +75,79 @@ let dot_of_cursor c =
   go_top c >>= function Cursor (_, n, _) -> return @@ dot_of_node n
 
 (* Bud -> Leaf and Bud -> Bud are invalid, but not excluded by the GADT *)
-let validate_node context node =
+let validate_node (type a b c) context (node : (a, b, c) node) =
   let rec aux 
-    : type a b c . [> `Bud | `Internal | `None] -> context -> (a, b, c) node -> (unit, string) result = 
-    fun parent context node ->
-    match node with
-    | Disk  (i, wit) -> 
-        let node = View (load_node context i wit) in
-        aux parent context node
-    | View v ->
-        match v with
-        | Leaf _ ->
-            begin match parent with
-            | `None -> Error "Leaf cannot be the root"
-            | `Bud -> Error "Leaf cannot have a Bud as the parent"
-            | `Internal | `Extender -> Ok ()
-            end
-        | Bud (child, _, _, _) ->
-            begin match parent with
-            | `Bud -> Error "Bud cannot have a Bud as the parent"
-            | `Internal | `Extender | `None ->
-                match child with
-                | None -> Ok ()
-                | Some node -> aux `Bud context node
-            end
-        | Internal (n1, n2, _, _, _) ->
-            begin match parent with
-            | `None -> Error "Internal cannot be the root"
-            | `Bud | `Internal | `Extender ->
-                aux `Internal context n1 >>= fun () ->
-                aux `Internal context n2
-            end
-        | Extender (seg, node, _, _, _) ->
-            if List.length (seg :> Path.side list) > 223 then 
-              Error "segment too long"
-            else begin 
-              match parent with
-              | `None -> Error "Extender cannot be the root"
-              | `Extender -> 
-                  (* This one should be checked already by the GADT *)
-                  Error "Extender cannot have an Extender as the parent"
-              | `Bud | `Internal -> aux `Extender context node
+    : type a b c . (a, b, c) node -> ((a, b, c) view, string) result = 
+    (* XXX Todo: No check of indexed_implies_hashed *)
+    fun node ->
+      let indexing_rule : type a b c . (a,b,c) view -> bool = function
+        | Internal (_, _, Indexed _, _, _) -> true
+        | Bud (_, Indexed _, _, _) -> true
+        | Leaf (_, Indexed _, _, _) -> true
+        | Extender (_, _, Indexed _, _, _) -> true
+        | _ -> false
+      in
+      let hashed_is_transitive : type a b c . (a,b,c) view -> bool = function
+        | Internal (_, _, _, Hashed _, _) -> true
+        | Bud (_, _, Hashed _, _) -> true
+        | Leaf (_, _, Hashed _, _) -> true
+        | Extender (_, _, _, Hashed _, _) -> true
+        | _ -> false
+      in
+      match node with
+      | Disk  (i, wit) -> aux @@ View (load_node context i wit) 
+      | View v -> 
+          match v with
+          | Leaf _ -> Ok v
+          | Bud (None, _, _, _) -> Ok v
+          | Bud (Some child, ir, hit, _iih) ->
+              begin
+                aux child >>= function
+                | Bud _ -> Error "Bud cannot carry Bud"
+                | Leaf _ -> Error "Bud cannot carry Leaf"
+                | v' ->
+                    (match ir, indexing_rule v' with
+                     | _, true -> Ok ()
+                     | Not_Indexed, _ -> Ok ()
+                     | _ -> Error "Bud: Strange indexing_rule") >>= fun () ->
+                    (match hit, hashed_is_transitive v' with
+                     | _, true -> Ok v
+                     | Not_Hashed, _ -> Ok v
+                     | _ -> Error "Bud: Strange hashed_is_transitive")
               end
+          | Internal (l, r, ir, hit, _iih) ->
+              begin
+                aux l >>= fun l ->
+                aux r >>= fun r -> 
+                (match ir, indexing_rule l, indexing_rule r with
+                 | _, true, true -> Ok ()
+                 | Left_Not_Indexed, false, _ -> Ok ()
+                 | Right_Not_Indexed, _, false -> Ok ()
+                 | Not_Indexed, _, _ -> Ok ()
+                 | Indexed _, _, _ -> Error "Internal: Strange indexing_rule"
+                 | Right_Not_Indexed, _, _ -> Error "Internal: Strange indexing_rule"
+                 | Left_Not_Indexed, _, _ -> Error "Internal: Strange indexing_rule") >>= fun () ->
+                (match hit, hashed_is_transitive l, hashed_is_transitive r with
+                 | _, true, true -> Ok v
+                 | Not_Hashed, _, _ -> Ok v
+                 | _ -> Error "Internal: Strange hashed_is_transitive")
+              end
+          | Extender (seg, node, ir, hit, _) ->
+              aux node >>= function
+              | Extender _ -> Error "Extender cannot carry Extender"
+              | v' ->
+                  if List.length (seg :> Path.side list) > 223 then 
+                    Error "segment too long"
+                  else 
+                    (match ir, indexing_rule v' with
+                     | _, true -> Ok ()
+                     | Not_Indexed, _ -> Ok ()
+                     | _ -> Error "Extender: Strange hashed_is_transitive") >>= fun () ->
+                    (match hit, hashed_is_transitive v' with
+                     | _, true -> Ok v
+                     | Not_Hashed, _ -> Ok v
+                     | _ -> Error "Extender: Strange hashed_is_transitive")
   in
-  aux `None context node
+  aux node >>= function
+  | Bud _ -> Ok ()
+  | _ -> Error "Tree must start with a Bud"
