518f00db732bc6627ad0fc42fef96d1601344c5c
Jun Furuse <jun.furuse@gmail.com>
2019-04-09 08:25:32 +0000

    Added module Cursor for cursor based tree manipulation

--- src/plebeia_intf.ml
+++ src/plebeia_intf.ml
@@ -34,41 +34,43 @@ module type S = sig
   (** Copies from the src context trees rooted in the hash list
       into a new context. Used for garbage collection. *)
   
-  val root : Context.t -> hash -> (cursor, error) result
-  (** Gets the root cursor corresponding to a given root hash in the
-      context. *)
+  module Cursor : sig
+    val root : Context.t -> hash -> (cursor, error) result
+    (** Gets the root cursor corresponding to a given root hash in the
+        context. *)
   
-  val empty : Context.t -> cursor
-  (** Creates a cursor to a new, empty tree. *)
-  
-  val subtree : cursor -> segment -> (cursor, error) result
-  (** Moves the cursor down a segment, to the root of a sub-tree. Think
-      "cd segment/" *)
-  
-  val create_subtree: cursor -> segment -> (cursor, error) result
-  (** Create a subtree (bud). Think "mkdir segment" *)
-  
-  val parent : cursor -> (cursor, error) result
-  (** Moves the cursor back to the parent tree. Think "cd .." *)
-  
-  val get : cursor -> segment -> (value, error) result
-  (** Gets a value if present in the current tree at the given
-      segment. *)
-  
-  val insert: cursor -> segment -> value -> (cursor, error) result
-  (** Inserts a value at the given segment in the current tree.
-      Returns the new cursor if successful. *)
-  
-  val upsert: cursor -> segment -> value -> (cursor, error) result
-  (** Upserts. This can still fail if the segment leads to a subtree. *)
-  
-  val delete: cursor -> segment -> (cursor, error) result
-  (** Delete a leaf or subtree. *)
-  
-  val snapshot: cursor -> segment -> segment -> (cursor, error) result
-  (** Snapshots a subtree at segment and place a soft link to it at
-      another segment location. *)
-  
+    val empty : Context.t -> cursor
+    (** Creates a cursor to a new, empty tree. *)
+    
+    val subtree : cursor -> segment -> (cursor, error) result
+    (** Moves the cursor down a segment, to the root of a sub-tree. Think
+        "cd segment/" *)
+    
+    val create_subtree: cursor -> segment -> (cursor, error) result
+    (** Create a subtree (bud). Think "mkdir segment" *)
+    
+    val parent : cursor -> (cursor, error) result
+    (** Moves the cursor back to the parent tree. Think "cd .." *)
+    
+    val get : cursor -> segment -> (value, error) result
+    (** Gets a value if present in the current tree at the given
+        segment. *)
+    
+    val insert: cursor -> segment -> value -> (cursor, error) result
+    (** Inserts a value at the given segment in the current tree.
+        Returns the new cursor if successful. *)
+    
+    val upsert: cursor -> segment -> value -> (cursor, error) result
+    (** Upserts. This can still fail if the segment leads to a subtree. *)
+    
+    val delete: cursor -> segment -> (cursor, error) result
+    (** Delete a leaf or subtree. *)
+    
+    val snapshot: cursor -> segment -> segment -> (cursor, error) result
+    (** Snapshots a subtree at segment and place a soft link to it at
+        another segment location. *)
+  end
+
   val commit: cursor -> (cursor * hash, cursor * index * hash * index) result
   (** Commits the change made in a cursor to disk. 
       The cursor must point to a root. 
@@ -82,5 +84,4 @@ module type S = sig
   
   val hash: cursor -> (cursor * hash)
   (** Computes the hash of the cursor without committing. *)
-
 end
