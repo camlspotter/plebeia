f4a5e06fa06ca6faf5b56e593e4951a961bf6ac4
Jun Furuse <jun.furuse@gmail.com>
2019-06-28 07:47:47 +0000

    Removed debug printfs

--- src/KVS.ml
+++ src/KVS.ml
@@ -1,3 +1,5 @@
+(* In memory KVS *)
+
 type t = (Hash.hash28, Value.t * int) Hashtbl.t
 
 let make () = Hashtbl.create 0
@@ -7,7 +9,11 @@ let close _ = ()
 let insert table h v =
   match Hashtbl.find_opt table h with
   | None -> Hashtbl.add table h (v, 1)
-  | Some (v', _) when v <> v' -> assert false (* XXX unfortunate hash collision! *)
+  | Some (v', _) when v <> v' -> 
+      (* XXX unfortunate hash collision! 
+         If this happens, we cannot store [v] in the KVS at all.
+      *)
+      assert false 
   | Some (_, count) -> Hashtbl.replace table h (v, count+1)
 
 let get_opt table h =
@@ -17,7 +23,6 @@ let get_opt table h =
 
 let decr table h =
   match Hashtbl.find_opt table h with
-  | None -> Printf.printf "none\n" ; ()
-  | Some (_, 1) -> Printf.printf "some 1\n" ; Hashtbl.remove table h
-  | Some (v, count) -> Printf.printf "some %d\n" count ;
-      Hashtbl.replace table h (v, count-1)
+  | None -> ()
+  | Some (_, 1) -> Hashtbl.remove table h
+  | Some (v, count) -> Hashtbl.replace table h (v, count-1)
